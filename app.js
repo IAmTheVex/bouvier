cd7c34dab1efa6a77492efeeef7142973acbaaca
NoName64Bits <ciobanulaur@gmail.com>
2017-02-06 17:24:29 +0200

    added Bouvier-Log

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 
 var bouvierOptions = {
   modules: [
+    ['log', require('bouvier-log'), {}],
     ['render', require('bouvier-engine'), {mainLayout: 'main'}],
     ['db', require('bouvier-db'), {db: "mongodb://127.0.0.1:27017/bouvier"}]
 //    ['local-login', require('bouvier-login-local'), {}]
@@ -19,9 +20,13 @@ var Cat = bouvier.module('db').registerModel('Cat', {name: String, age: Number})
 
 
 app.get('/', (req, res) => {
-  Cat.find({age: {$gte: 4}}, (err, cats) => {
+  Cat.find({}, ['name', 'age'], {sort: {name: 1}}, (err, cats) => {
     res.render('home', {cats: cats});
   });
+});
+
+app.get('/hel', (req, res) => {
+  res.json({api: "apii"});
 });
 
 app.listen(3000);
